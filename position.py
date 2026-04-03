38bf4ad9110d4c6351e9dc3123df95607003be84
TheophileSegui <seguitheophile@gmail.com>
2026-04-03 09:30:36 +0200

    Final untested version of the class Position, with a fuction to go from column and row to a numeric tuple (x,y)

--- position.py
+++ position.py
@@ -1,45 +1,38 @@
 #the position class file
 
 class Position:
-    def __init__(self,numeric = (0,0),pos = "a8"):
-        self._numeric = numeric
-        self._pos = pos
-    @property
-    def numeric(self) -> tuple:
-        return self._numeric
+    def __init__(self):
+        self.column = "a"
+        self.row = 1
+        self.numeric = (0,0)
 
-    @numeric.setter
-    def numeric(self,value):
-        self._numeric = value
-    @property
-    def pos(self) -> str:
-        return self._pos
-    @pos.setter
-    def pos(self,value):
-        self._pos = value
+    def __str__(self):
+        return self.column + str(self.row)
 
-    def toPos(self,value):
+    @property
+    def column(self):
+        return self.column
+
+    @column.setter
+    def column(self, value):
         if type(value) == str:
-            alphabet = ["a","b","c","d","e","f","g","h"]
-            dictNumber={}
-            dictLetter={}
-            for i in range(9):
-                dictNumber[str(i)] = i
-            for i in range(9):
-                dictLetter[alphabet[i]] = i
-        else:
-            raise ValueError(f"The given position has to be of type 'str', given type was {type(value)}")
+            self.column = value
+            if "a"<=value<="h":
+                self.column = value
+                self.numeric = self.toNumeric()
+    @property
+    def row(self):
+        return self.row
 
+    @row.setter
+    def row(self, value):
+        if type(value) == int:
+            self.row = value
 
-
-
-
-    def changePos(self,value):
-        if type(value) == tuple:
-            if 0 <= value[0] < 9 and 0 < value[1] < 9:
-                self._numeric = value
-                self._pos = self.toPos(value)
-        elif type(value) == str:
-            if "a" <= value[0] <= "h" and "0" <= value[1] < "9":
-                self._pos = value
-                self._numeric = self.toNumeric(value)
+    def toNumeric(self):
+        letter = ["a","b","c","d","e","f","g","h"]
+        dictLetter = {}
+        for i in range(8):
+            dictLetter[letter[i]] = i
+        rlt = (dictLetter[self.column],self.row-1)
+        return rlt
